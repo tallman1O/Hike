18d196a6294c05f0ea895c99a95e48f8a209ff94
Mehul Uttam <mehulsavio@gmail.com>
2025-07-16 09:15:45 +0530

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Mehul Uttam on 16/07/25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
